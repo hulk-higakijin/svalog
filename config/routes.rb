7130092c41e6e521171ff75f04cdf821bacc0a00
hulk-higakijin <hm385.chejptks@gmail.com>
2023-04-06 22:03:18 +0900

    feat: create profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'profiles/show'
+  get 'profiles/edit'
   root 'home#index'
+  devise_for :users
+
+  resource :profile, only: %i[show edit update]
+  resolve('Profile') { [:profile] }
 end
