4fb7274a2f135562d67e3ffb0dede0a702f40e29
hulk-higakijin <hm385.chejptks@gmail.com>
2023-04-06 20:02:51 +0900

    chore: flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
+  plugins: [require("daisyui")],
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  daisyui: {
+    themes: ["light"]
+  }
 }
