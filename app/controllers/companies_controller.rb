870208fc9e071ce3db0215e96b3054cc533f6af5
hulk-higakijin <hm385.chejptks@gmail.com>
2023-04-15 14:54:19 +0900

    feat: create companies#new

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,29 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :edit, :update]
+
   def show; end
 
-  def new; end
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
+  end
 
   def edit; end
 
   private
+
+    def company_params
+      params.require(:company).permit(:name, :address)
+    end
 
     def set_company
       @company = Company.find(params[:id])
