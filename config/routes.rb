ec9055fe9f94df0466a9316ce62ca9594d35d28e
hulk-higakijin <hm385.chejptks@gmail.com>
2023-04-15 14:31:24 +0900

    feat: create companise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
-  get 'profiles/edit'
   root 'home#index'
   devise_for :users
+
+  resources :companies, only: %i[show new create edit update]
 
   resource :profile, only: %i[show edit update]
   resolve('Profile') { [:profile] }
