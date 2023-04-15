24c6ddc28907dca7d93180495a994c40d531e761
hulk-higakijin <hm385.chejptks@gmail.com>
2023-04-15 15:49:04 +0900

    feat: delete profiles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :companies, only: %i[show new create edit update]
-
-  resource :profile, only: %i[show edit update]
-  resolve('Profile') { [:profile] }
 end
