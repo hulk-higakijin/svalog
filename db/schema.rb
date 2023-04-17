bbd10e71e504a0fff5797db05cc79cd37dce8ed6
hulk-higakijin <hm385.chejptks@gmail.com>
2023-04-17 13:14:08 +0900

    feat: enable_extension 'pgcrypto' to run github actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_061414) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_041347) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "companies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
