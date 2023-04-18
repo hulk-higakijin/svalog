c6edca54202cd9195e7daa07b7a0e5bd895427b0
hulk-higakijin <hm385.chejptks@gmail.com>
2023-04-18 14:54:42 +0900

    feat: go to next & last month

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,9 +1,10 @@
 class ShiftsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_year_and_month
+  before_action :set_date
 
   def show
-    @shift = current_user.shifts
+    @shifts = current_user.shifts
   end
 
   private
@@ -11,5 +12,9 @@ class ShiftsController < ApplicationController
     def set_year_and_month
       @year = params[:year].to_i.zero? ? this_year : params[:year].to_i
       @month = params[:month].to_i.zero? ? this_month : params[:month].to_i
+    end
+
+    def set_date
+      @date = Date.new(@year, @month)
     end
 end
